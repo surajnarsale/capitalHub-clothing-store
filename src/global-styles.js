4e1134c23646f6b3bb3c6e9840fb5b31f56babaa
Suraj Narsale <surajsnarsale@gmail.com>
2021-02-14 10:46:26 +0530

    feat: add global styles

---
+++ src/global-styles.js
@@ -0,0 +1,100 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+:root {
+  --clr-primary:#22d1ee;
+  --clr-secondary:#0e153a;
+  --clr-tertiary:#f4f7f8;
+  --clr-dark:#000000;
+  --clr-light:#f5f5f5;
+
+  --ff-primary: 'Calibri',monospace;
+  --ff-secondary: 'Public Sans', sans-serif;
+  --fs-h1: 3rem;
+  --fs-h2: 1.55rem;
+  --fs-h3: 1.25rem;
+  --fs-h4: 0.875rem;
+  --fs-body: 1.25rem;
+  --spacing: 0.1rem;
+
+  --fw-reg: 300;
+  --fw--semibold: 600;
+  --fw-bold: 900;
+
+  --transition: all 0.3s linear;
+  --light-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+  --dark-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+
+  --mobile:576px;
+  --tablet:768px;
+  --laptop:992px;
+  --desktop:1200px;
+}
+*,
+::after,
+::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+  margin: 0;
+  padding: 0;
+  background: var(--clr-tertiary);
+  color: var(--clr-secondary);
+  font-family:var(--ff-primary) ;
+  }
+  ul {
+    list-style-type: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+h1,
+h2,
+h3,
+h4 {
+    letter-spacing: var(--spacing);
+    line-height: 1.25;
+}
+
+h1 {
+    font-size: var(--fs-h1);
+}
+
+h2 {
+    font-size: var(--fs-h2);
+}
+
+h3 {
+    font-size: var(--fs-h3);
+}
+
+h4 {
+    font-size: var(--fs-h4);
+}
+
+img {
+    display: block;
+    max-width: 100%;
+}
+
+@media screen and (min-width: var(--tablet)) {
+    :root {
+        --fs-h1: 4.5rem;
+        --fs-h2: 3.75rem;
+        --fs-h3: 1.5rem;
+        --fs-h4: 1rem;
+        --fs-body: 1.125rem;
+    }
+    h1,
+    h2,
+    h3,
+    h4 {
+        line-height: 1;
+    }
+}
+
+`;
